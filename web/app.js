4ed531d3e06566652f20de76935b68cf2628ea0f
Hunter-Lin <Linkin_HearT@sjtu.edu.cn>
2016-03-14 23:42:48 +0800

    web backend

--- web/app.js
+++ web/app.js
@@ -1,15 +1,29 @@
 var express = require('express');    
-var app = express();   
+var fs = require('fs');
+var path = require('path')
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/hello/*', function(req, res){  
     console.log(req.query.name);  
     console.log(req.query.email);  
     res.send('Get Over');    
-});    
-  
-app.get('/', function(req, res){  
-res.render('index');  
-});  
-  
+});
+
+app.get('/', function(req, res){
+	// console.log(req);
+	fs.readFile(__dirname + "/index.html", 'utf8', function (err, data) {
+       res.end(data);
+   });
+});
+
+app.post('/visual', function(req, res){
+
+	fs.readFile(__dirname + '/visual/visual.html', 'utf8', function(err, data) {
+		// console.log(data);
+		res.send(data);
+	});
+});
+
 app.listen(8080);    
-console.log('Listening on port 8080'); 
+console.log('Listening on port 8080');  
