cf69f2d973b4b654fff712504679916715decd40
Hunter-Lin <Linkin_HearT@sjtu.edu.cn>
2016-03-15 15:55:45 +0800

    bridge the web and mysql

--- web/app.js
+++ web/app.js
@@ -1,29 +1,63 @@
-var express = require('express');    
-var fs = require('fs');
-var path = require('path')
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/hello/*', function(req, res){  
-    console.log(req.query.name);  
-    console.log(req.query.email);  
-    res.send('Get Over');    
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , mysql = require('mysql')
+  , partials = require('express-partials')
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(partials());
+  app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/', function(req, res){
-	// console.log(req);
-	fs.readFile(__dirname + "/index.html", 'utf8', function (err, data) {
-       res.end(data);
-   });
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// get mysql running!
+var client = mysql.createConnection({
+  user: 'root',
+  password: ''
+});
+client.connect();
+client.query('show databases', function(err, res, fields) {
+  if (err) { throw err; }
+  // console.log(res);
+});
+
+
+// Routes
+
+app.get('/', routes.index);
 
 app.post('/visual', function(req, res){
-
-	fs.readFile(__dirname + '/visual/visual.html', 'utf8', function(err, data) {
-		// console.log(data);
-		res.send(data);
-	});
+  graph = JSON.stringify(req.body);
+  console.log(graph);
+  // graph = req.body;
+  // graph = '{title: "test"}';
+  // then we can access mysql to transform the value into json data.
+  res.render('visual', {
+    layout: '',
+    graph: '{title: "test"}'
+  });
 });
 
-app.listen(8080);    
-console.log('Listening on port 8080');  
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
